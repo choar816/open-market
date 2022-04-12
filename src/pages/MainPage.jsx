0ecb4d7cf9a8424833757b468017556fc0d2ae24
Ahra Cho <choar816@postech.ac.kr>
2022-04-13 01:09:50 +0900

    Add: image carousel to MainPage

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 import Header from '../components/Header';
+import Carousel from '../components/Carousel';
 
 function MainPage() {
   return (
     <Container>
-      <Header></Header>
+      <Header />
+      <Carousel />
     </Container>
   )
 }
