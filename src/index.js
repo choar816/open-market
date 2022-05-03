7a56c9f658a41dc34ece7e093ed2d7b03f0ba30f
Ahra Cho <choar816@postech.ac.kr>
2022-05-03 14:31:59 +0900

    Fix: deploy error

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter basename="">
+ReactDOM.render(
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>,
+  document.getElementById('root'),
 );
