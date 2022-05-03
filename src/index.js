6aaeae8567933f1b7ee1dbe408760cd9a56b0686
vince.joe <vince.joe@kakaocorp.com>
2022-05-03 21:56:39 +0900

    test

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import App from './App';
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App />
+  </BrowserRouter>,
 );
