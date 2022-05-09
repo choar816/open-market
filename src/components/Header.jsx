2cc5fce14a7aac9fa3119247c9d9c9bec9aea756
Ahra Cho <choar816@postech.ac.kr>
2022-05-09 18:46:22 +0900

    Add: MypageMenu in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import SearchBar from './SearchBar';
 import IconButton from './button/IconButton';
@@ -8,10 +8,10 @@ import ImgUser from '../../public/assets/icon-user.svg';
 import ImgBag from '../../public/assets/icon-shopping-bag.svg';
 import ColorIconButton from './button/ColorIconButton';
 import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
 
 function Header({ buyer }) {
   const navigate = useNavigate();
+  const [showMenu, setShowMenu] = useState(false);
 
   return (
     <Container>
@@ -21,8 +21,28 @@ function Header({ buyer }) {
       </SubContainer>
       <SubContainer right>
         {buyer || <IconButton src={ImgCart}>장바구니</IconButton>}
-        <StyledLink to="/login"><IconButton src={ImgUser}>마이페이지</IconButton></StyledLink>
-        {buyer && <ColorIconButton iconSrc={ImgBag}>판매자 센터</ColorIconButton>}
+        {!localStorage.getItem('token') ? (
+          <>
+            <IconButton
+              src={ImgUser}
+              onClick={() => navigate('/mypage')}
+              children="마이페이지">
+            </IconButton>
+            <MypageMenu>
+              <li>마이페이지</li>
+              <li>로그아웃</li>
+            </MypageMenu>
+          </>
+        ) : (
+          <IconButton
+            src={ImgUser}
+            onClick={() => navigate('/login')}
+            children="로그인"
+          />
+        )}
+        {buyer && (
+          <ColorIconButton iconSrc={ImgBag}>판매자 센터</ColorIconButton>
+        )}
       </SubContainer>
     </Container>
   );
@@ -72,10 +92,6 @@ const SubContainer = styled.article`
   }
 `;
 
-const StyledLink = styled(Link)`
-  margin-left: 20px;
-`;
-
 const Logo = styled.img`
   width: 124px;
   object-fit: contain;
@@ -83,4 +99,32 @@ const Logo = styled.img`
   @media screen and (max-width: 768px) {
     width: 80px;
   }
+`;
+
+const MypageMenu = styled.ul`
+  position: absolute;
+  top: 90px;
+  right: 15px;
+  width: 120px;
+  border-radius: 10px;
+  background-color: #fff;
+  box-shadow: 0px 0px 10px 4px rgba(0,0,0,0.2);
+  padding: 10px;
+
+  li {
+    padding: 2px 5px;
+    text-align: center;
+    color: #767676;
+    border: 1px solid transparent;
+    border-radius: 5px;
+    transition: all 0.5s;
+    & + li {
+      margin-top: 10px;
+    }
+    &:hover {
+      color: #000;
+      border: 1px solid #767676;
+    }
+  }
+
 `;
