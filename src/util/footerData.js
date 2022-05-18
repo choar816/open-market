7fbd07f5ebc326ba606eec4a0853f99a4be0edb3
Ahra Cho <choar816@postech.ac.kr>
2022-05-18 13:10:34 +0900

    Add: util/carousel, util/footerData

---
+++ src/util/footerData.js
@@ -0,0 +1,55 @@
+import ImgInsta from '../../public/assets/icon-insta.svg';
+import ImgFb from '../../public/assets/icon-fb.svg';
+import ImgYoutube from '../../public/assets/icon-yt.svg';
+
+export const links = [
+  {
+    desc: '호두샵 소개',
+    url: 'https://choar816.github.io/intro-choar/',
+  },
+  {
+    desc: '이용약관',
+    url: 'https://github.com/choar816',
+  },
+  {
+    desc: '개인정보처리방침',
+    url: 'https://choar816.github.io/intro-choar/',
+  },
+  {
+    desc: '전자금융거래약관',
+    url: 'https://github.com/choar816',
+  },
+  {
+    desc: '청소년보호정책',
+    url: 'https://choar816.github.io/intro-choar/',
+  },
+  {
+    desc: '제휴문의',
+    url: 'https://github.com/choar816',
+  },
+];
+
+export const socials = [
+  {
+    desc: 'instagram',
+    url: 'https://www.instagram.com/',
+    imgSrc: ImgInsta,
+  },
+  {
+    desc: 'facebook',
+    url: 'https://www.facebook.com/',
+    imgSrc: ImgFb,
+  },
+  {
+    desc: 'youtube',
+    url: 'https://www.youtube.com/',
+    imgSrc: ImgYoutube,
+  },
+];
+
+export const companyInfo = [
+  '(주)HODU SHOP',
+  '제주특별자치도 제주시 동광고 137 제주코딩베이스캠프',
+  '사업자 번호 : 000-0000-0000 | 통신판매업',
+  '대표 : 김호두',
+];
