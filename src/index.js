a960e9f4ed741a30e12acd93b68cd9fc8af2d3ff
Ahra Cho <choar816@postech.ac.kr>
2022-06-22 15:43:41 +0900

    Add: react-query (ProductList, ProductContent)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import App from './App';
+
+const queryClient = new QueryClient();
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
   <HashRouter basename={process.env.PUBLIC_URL}>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={true} />
+      <App />
+    </QueryClientProvider>
   </HashRouter>,
 );
