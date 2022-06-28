1176e2c38a4314888937e91c4b4e9368f59f74ba
Ahra Cho <choar816@postech.ac.kr>
2022-06-28 13:59:34 +0900

    Edit: integrate some components

--- src/components/TabBar.js
+++ src/components/TabBar.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
-import TabButton from './TabButton';
+
+const TabButton = ({ onClick, chosen, children }) => {
+  return <Button onClick={onClick} chosen={chosen} children={children} />;
+};
 
 const TabBar = ({ tabList, chosenTab, setChosenTab }) => {
   return (
@@ -23,4 +26,23 @@ export default TabBar;
 const Container = styled.article`
   margin-bottom: 30px;
   display: flex;
+`;
+
+const Button = styled.button`
+  width: 320px;
+  padding: 18px 0 12px;
+  font-weight: 700;
+  font-size: 18px;
+  color: #21bf48;
+  background-color: #ffffff;
+  border: none;
+  border-bottom: 6px solid #21bf48;
+
+  ${({ chosen }) =>
+    !chosen &&
+    `
+    font-weight: 500;
+    color: #767676;
+    border-bottom: 6px solid #E0E0E0;
+  `}
 `;
