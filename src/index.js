37fbb78a004a3d842c1d23751e88b3bf515f5c03
Ahra Cho <choar816@postech.ac.kr>
2022-09-11 22:56:14 +0900

    Edit: HashRouter -> BrowerRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import App from './App';
@@ -10,10 +10,10 @@ const queryClient = new QueryClient();
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-  <HashRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={true} />
       <App />
     </QueryClientProvider>
-  </HashRouter>,
+  </BrowserRouter>,
 );
